a86bc6544751e57f9723d8a7177064d29f1bc083
mirislam486 <34451472+mirislam486@users.noreply.github.com>
2018-10-09 09:29:55 -0400

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,19 @@
+public class BankAccount{
+  private double balance;
+  private int accountID;
+  private String password;
+
+  public BankAccount(double ibalance, int iaccountid, String ipassword){
+    balance = ibalance;
+    accountID = iccountid;
+    password = ipassword;
+  }
+
+  public double returnBalance(){
+    return balance;
+  }
+
+  public returnID(){
+    return ac
+  }
+}
